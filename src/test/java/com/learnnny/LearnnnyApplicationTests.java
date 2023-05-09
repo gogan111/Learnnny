6e6e45212840ede0fb0e7b1ecf98bc93aff63e31
Syniaokiy <andrew.syniaokii@gmail.com>
2023-05-09 12:55:30 +0300

    Initial commit

---
+++ src/test/java/com/learnnny/LearnnnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnnny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnnnyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
